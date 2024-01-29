12a2b43a09445137f51556dd6365f7d5e0764dd7
FerNIX1 <edgarfernandolopez001@gmail.com>
2024-01-28 23:50:07 -0600

    cambie nombre de archivo login

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+
+import { Login } from "../components/Login/Login"
+import { Navbar } from "../components/Navbar/Navbar"
+export const LoginPage = () => {
+  return (
+    <>
+    <Navbar></Navbar>
+    <Login></Login>
+    </>
+  )
+}
