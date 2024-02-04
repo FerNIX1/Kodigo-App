c4a97fd91e02e446508579c18a883d361e2a6dc6
Mirna <mirnarecinos382@gmail.com>
2024-02-04 17:44:06 +0530

    Responsive cards

--- src/components/aliados/Aliados.jsx
+++ src/components/aliados/Aliados.jsx
@@ -7,10 +7,10 @@ export const Aliados = () => {
       <span className='aliados'>Nuestros aliados</span>
       </div>
    <div className='container'>
-    <img src="https://res.cloudinary.com/dzb6jy2uq/image/upload/v1706754747/Captura_de_pantalla_910_noih1w.png" style={{ objectFit: "cover" }} width="96%" alt="" />
+    <img className='imgFluid img-aliados' src="https://res.cloudinary.com/dzb6jy2uq/image/upload/v1706754747/Captura_de_pantalla_910_noih1w.png" style={{ objectFit: "cover" }} width="96%" alt="" />
    </div>
 
     
     </>
   )
-}
+}                                                                                                                                       
